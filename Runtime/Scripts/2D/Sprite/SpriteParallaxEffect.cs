36816ae1aa2bfca507c650a07041ae693a820c70
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-09-19 14:03:21 -0500

    simplified parallax

--- Runtime/Scripts/2D/Sprite/SpriteParallaxEffect.cs
+++ Runtime/Scripts/2D/Sprite/SpriteParallaxEffect.cs
@@ -6,43 +6,28 @@ namespace Chonker2D.Sprite {
         [SerializeField, Tooltip("Most likely the Main Camera")]
         private Transform TargetMovementReference;
 
-        [SerializeField] private bool _syncYPosition;
 
         [SerializeField, Range(0, 1)] private float parallaxAmountX = .5f;
-        [SerializeField, Range(-1, 1)] private float parallaxAmountY = .5f;
+        [SerializeField, Range(0, 1)] private float parallaxAmountY = .5f;
         private Vector2 targetPosition;
         private float startXPos;
         private float startYPos;
-        private float minParallax = .8f;
-        private float maxParallax = 1;
 
         private void Awake() {
         }
 
         private void Start() {
             startXPos = TargetMovementReference.position.x;
-            startYPos = TargetMovementReference.position.y;
-
-            float yPos = transform.position.y;
-            if (_syncYPosition && TargetMovementReference) {
-                yPos = TargetMovementReference.position.y;
-            }
-            
-            targetPosition = new Vector2(startXPos,  yPos);
+            startYPos = transform.position.y;
         }
 
         private void Update() {
-            float targetMovementReferenceXPos = TargetMovementReference.transform.position.x;
-            float targetMovementReferenceYPos = TargetMovementReference.transform.position.y;
-            float distanceToStartX = targetMovementReferenceXPos - startXPos;
-            float distanceToStartY = targetMovementReferenceYPos - startYPos;
-            float parallaxAmountFinalX = Mathf.Lerp(maxParallax, minParallax, parallaxAmountX);
-            float parallaxAmountFinalY = Mathf.Lerp(maxParallax, minParallax, parallaxAmountY);
-            targetPosition.x = startXPos + distanceToStartX * parallaxAmountFinalX;
-            targetPosition.y = startYPos + distanceToStartY * parallaxAmountFinalY;
-            if (_syncYPosition) {
-                targetPosition.y = TargetMovementReference.position.y;
-            }
+            float distanceToStartX = TargetMovementReference.transform.position.x - startXPos;
+            float distanceToStartY = TargetMovementReference.transform.position.y - startYPos;
+            float parallaxAmountFinalX = distanceToStartX * parallaxAmountX;
+            float parallaxAmountFinalY = distanceToStartY * parallaxAmountY;
+            targetPosition.x = startXPos + parallaxAmountFinalX;
+            targetPosition.y = startYPos  + parallaxAmountFinalY;
             transform.position = targetPosition;
         }
     }
